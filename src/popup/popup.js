0d8cc382e5a3d6596e446f67bfa5ad0fbd1d7210
tanimotohikari <tanimoto.hikari@gmail.com>
2019-04-09 09:27:51 +0900

    update:feat2 seとbgmを追加

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,11 +1,8 @@
 import Vue from 'vue';
 import App from './App';
-import store from '../store';
-import router from './router';
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
   render: h => h(App),
 });
--- src/popup/router/pages/Index.vue
+++
@@ -1,8 +0,0 @@
-<template>
-<div class='contents'>
-  <button @click='startBattle'>戦闘開始!!</button>
-  <div>
-    こここここここっこk
-  </div>
-</div>
-</template>
